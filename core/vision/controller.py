9bd8bd01d2fa9285be981ab18e3a17299e2dbc58
PabloXantini <santinypabloe130@gmail.com>
2026-05-19 08:50:16 -0600

    chore: updated ignore files

--- core/vision/controller.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from .cv import HandDetector
-
-class Controller(ABC):
-    def __init__(self):
-        super().__init__()
-    @abstractmethod
-    def update(self, image, ctx:HandDetector, debug:bool):
-        pass    
